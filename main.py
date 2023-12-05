4181b2742c296e74dd097505b6cfeed4f047fd69
Replit user <>
2023-12-05 17:11:32 +0000

    Add files

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello Mahim\'s friends...'
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=True
+    )
